3ddcf1ab5de6dd967acdd74bb0f3d7315592a8e1
AndrewBaik <andrewbaik@outlook.com>
2018-06-07 10:08:10 -0700

    Testing using Visual Studio

---
+++ Challenges/Array_adjacent_product/Array_adjacent_product/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Array_adjacent_product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[,] { {1,2,1}, {2,4,2}, {3,6,8}, {7,8,1}};
+            //int output = 
+                //Adjacentproduct(arr);
+
+            // Console.WriteLine(output);
+            //Console.ReadLine();
+
+            int highestProductValue = 0;
+
+            for (int i = 1; i <= arr.GetLength(0) - 2; i++)
+            {
+                for (int j = 1; j <= arr.GetLength(1) - 2; j++)
+                {
+                    int productValue = CheckHighestProduct(arr, i, j);
+                   
+                    if (highestProductValue < productValue)
+                    {
+                        highestProductValue = productValue;
+                    }
+                }
+
+            }
+            Console.WriteLine(highestProductValue);
+            Console.ReadLine();
+        }
+      
+        static int CheckHighestProduct(int[,] arr, int i, int j)
+        {
+            int highestProduct = 0;
+            int productAmount = 0;
+            for(int a = i - 1; a <= i + 1; a++)
+            {
+                for(int e = j - 1; e <= j + 1; e++)
+                {
+                    if(!((a == i) && (e == j)))
+                    {
+                       /* Console.WriteLine($"i is {i} and j is {j}");
+                        Console.WriteLine(arr[i, j]);
+                        Console.WriteLine(arr[a, e]);
+                        Console.ReadLine();*/
+
+                        productAmount = arr[i, j] * arr[a, e];
+
+                        if(productAmount > highestProduct)
+                        {
+                            highestProduct = productAmount;
+                        }
+                    }                  
+                }
+            }
+            return highestProduct;
+        }
+    }
+}  
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Data Structures and Algorithms
 
 ## Completed DSA Challenges and Implementations
-1. [Reverse Array](/Challenge_ReverseArray/Reverse-Array/README.md)
-2. [Binary Search](/Challenge02_BinarySearch/README.md)
+## Table of Contents
+1. [Reverse Array](Challenges/Challenge_ReverseArray/Reverse-Array/README.md)
+2. [Binary Search](Challenges/Challenge02_BinarySearch/Program.cs/README.md)
+3. [Adjacent Array](Challenges/Array_adjacent_product/Array_adjacent_product/README.md)
