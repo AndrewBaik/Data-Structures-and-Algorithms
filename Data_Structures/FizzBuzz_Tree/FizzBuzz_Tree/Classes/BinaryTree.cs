5f1909eb0295ae60111705792390e46b164afa30
AndrewBaik <andrewbaik@outlook.com>
2018-06-26 23:41:10 -0700

    Fizz Buzz Tree DSA completed with test and code comments

---
+++ Data_Structures/FizzBuzz_Tree/FizzBuzz_Tree/Classes/BinaryTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz_Tree.Classes
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public BinaryTree(Node node)
+        {
+            Root = node;
+        }
+    }
+}
